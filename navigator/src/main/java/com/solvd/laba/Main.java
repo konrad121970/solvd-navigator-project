1cce1f5adb00c951a3ea32b3d04d1993694f231a
konrad121970 <konrad121970@windowslive.com>
2024-01-19 11:34:24 +0100

    Add basic main method to check if CItyREpository works

--- navigator/src/main/java/com/solvd/laba/Main.java
+++ navigator/src/main/java/com/solvd/laba/Main.java
@@ -1,7 +1,19 @@
 package com.solvd.laba;
 
+import com.solvd.laba.model.City;
+import com.solvd.laba.persistence.CityRepository;
+import com.solvd.laba.persistence.impl.CityRepositoryImpl;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        CityRepository cityRepository = new CityRepositoryImpl();
+
+        City city = new City("Bialowieza", 12.0,13.0, null);
+
+        cityRepository.create(city);
+
+        cityRepository.deleteById(city.getId());
+
     }
 }
