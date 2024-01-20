7075aec587b74078b3dda4d0c44859a91ddb88ee
konrad121970 <konrad121970@windowslive.com>
2024-01-20 14:56:54 +0100

    Basic implementation of services

--- navigator/src/main/java/com/solvd/laba/service/city/impl/CityService.java
+++ navigator/src/main/java/com/solvd/laba/service/city/impl/CityService.java
@@ -1,6 +1,17 @@
 package com.solvd.laba.service.city.impl;
 
+import com.solvd.laba.model.Road;
+import com.solvd.laba.persistence.city.CityRepository;
+import com.solvd.laba.persistence.city.impl.CityRepositoryImpl;
 import com.solvd.laba.service.city.ICityService;
 
 public class CityService implements ICityService {
+
+    private CityRepository cityRepository;
+    private RoadService roadService;
+
+    public CityService() {
+        cityRepository = new CityRepositoryImpl();
+        roadService = new RoadService();
+    }
 }
--- navigator/src/main/java/com/solvd/laba/service/city/impl/RoadService.java
+++ navigator/src/main/java/com/solvd/laba/service/city/impl/RoadService.java
@@ -1,6 +1,17 @@
 package com.solvd.laba.service.city.impl;
 
+import com.solvd.laba.persistence.city.RoadRepository;
+import com.solvd.laba.persistence.city.impl.RoadRepositoryImpl;
 import com.solvd.laba.service.city.IRoadService;
 
 public class RoadService implements IRoadService {
+
+    private RoadRepository roadRepository;
+    private CityService cityService;
+
+    public RoadService() {
+        roadRepository = new RoadRepositoryImpl();
+        cityService = new CityService();
+    }
+
 }
