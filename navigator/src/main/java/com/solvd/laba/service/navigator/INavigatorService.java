66afb813a42748cc420564787d0f6aa1a61ed678
konrad121970 <konrad121970@windowslive.com>
2024-01-20 15:12:52 +0100

    Create navigatorService

---
+++ navigator/src/main/java/com/solvd/laba/service/navigator/INavigatorService.java
@@ -0,0 +1,11 @@
+package com.solvd.laba.service.navigator;
+
+import com.solvd.laba.model.City;
+
+import java.util.List;
+
+public interface INavigatorService {
+    List<City> findShortestPath(City startCity, City endCity);
+
+    List<City> findShortestPathWithStop(City startCity, City intermediateCity, City endCity);
+}
