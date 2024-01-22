424ef3fb856287ee0043740f9043989e96755676
konrad121970 <konrad121970@windowslive.com>
2024-01-22 23:16:59 +0100

    Basic implementation of NavigatorService

--- navigator/src/main/java/com/solvd/laba/service/navigator/impl/NavigatorService.java
+++ navigator/src/main/java/com/solvd/laba/service/navigator/impl/NavigatorService.java
@@ -7,23 +7,34 @@ import com.solvd.laba.service.city.impl.CityService;
 import com.solvd.laba.service.city.impl.RoadService;
 import com.solvd.laba.service.navigator.INavigatorService;
 
-import java.util.List;
+import java.util.*;
 
 public class NavigatorService implements INavigatorService {
 
+    private Graph graph;
     private ICityService cityService;
-    private IRoadService roadService;
+//    private IRoadService roadService;
 
     public NavigatorService() {
         cityService = new CityService();
-        roadService = new RoadService();
+//        roadService = new RoadService();
+
+        graph = new Graph();
+        graph.addCities(cityService.getAllCities());
     }
 
     @Override
-    public List<City> findShortestPath(City startCity, City endCity){
-        return null;
+    public List<City> findShortestPath(City startCity, City endCity) {
+        // TODO: add savedRoutesService
+//        if(savedRoutesService.findByIds(startCity, endCity) != null){
+//
+//        }
+//        else {
+            return graph.findShortestPath(startCity.getId(), endCity.getId());
+//        }
     }
 
+    // TODO: implement findShortestPathWithStop() method
     @Override
     public List<City> findShortestPathWithStop(City startCity, City intermediateCity, City endCity){
         return null;
