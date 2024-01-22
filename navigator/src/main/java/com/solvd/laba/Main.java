17abf5096349617f9793ea239cadfea96da8d160
konrad121970 <konrad121970@windowslive.com>
2024-01-22 23:17:30 +0100

    Work with navigator service in main class

--- navigator/src/main/java/com/solvd/laba/Main.java
+++ navigator/src/main/java/com/solvd/laba/Main.java
@@ -1,25 +1,24 @@
 package com.solvd.laba;
 
 import com.solvd.laba.model.City;
-import com.solvd.laba.model.Road;
-import com.solvd.laba.persistence.city.CityRepository;
-import com.solvd.laba.persistence.city.RoadRepository;
-import com.solvd.laba.persistence.city.impl.CityRepositoryImpl;
-import com.solvd.laba.persistence.city.impl.RoadRepositoryImpl;
+import com.solvd.laba.service.city.ICityService;
+import com.solvd.laba.service.city.impl.CityService;
+import com.solvd.laba.service.navigator.INavigatorService;
+import com.solvd.laba.service.navigator.impl.NavigatorService;
+
+
 
 public class Main {
     public static void main(String[] args) {
+        ICityService cityService = new CityService();
+        INavigatorService navigatorService = new NavigatorService();
 
-        CityRepository cityRepository = new CityRepositoryImpl();
-        RoadRepository roadRepository = new RoadRepositoryImpl();
+        City city1 = cityService.findCityById(1L);
+        City city2 = cityService.findCityById(5L);
 
-        City city = new City("Bialowieza", 12.0,13.0, null);
-        cityRepository.create(city);
-        cityRepository.deleteById(city.getId());
+        System.out.println(navigatorService.findShortestPath(city1, city2));
 
-        Road road = new Road(1L,4L, 200);
-        roadRepository.create(road);
-        roadRepository.deleteRoad(road.getStartCityId(), road.getEndCityId());
+        System.out.println();
 
     }
 }
