67b8380bc26756d2fb73c2c09ab4b70d3d6995f3
konrad121970 <konrad121970@windowslive.com>
2024-01-24 17:22:13 +0100

    Add RouteCityService

---
+++ navigator/src/main/java/com/solvd/laba/service/route/IRouteCityService.java
@@ -0,0 +1,4 @@
+package com.solvd.laba.service.route;
+
+public interface IRouteCityService {
+}
---
+++ navigator/src/main/java/com/solvd/laba/service/route/impl/RouteCityService.java
@@ -0,0 +1,6 @@
+package com.solvd.laba.service.route.impl;
+
+import com.solvd.laba.service.route.IRouteCityService;
+
+public class RouteCityService implements IRouteCityService {
+}
