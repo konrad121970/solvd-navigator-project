36608ef5ee55155e42e38512527eb42d5bd7f478
konrad121970 <konrad121970@windowslive.com>
2024-01-24 23:25:17 +0100

    Implement searching for saved routes and saving route in findShortestPath method

--- navigator/src/main/java/com/solvd/laba/service/navigator/impl/NavigatorService.java
+++ navigator/src/main/java/com/solvd/laba/service/navigator/impl/NavigatorService.java
@@ -1,27 +1,68 @@
 package com.solvd.laba.service.navigator.impl;
 
 import com.solvd.laba.model.City;
+import com.solvd.laba.model.Route;
 import com.solvd.laba.service.city.ICityService;
+import com.solvd.laba.service.city.IRoadService;
 import com.solvd.laba.service.city.impl.CityService;
+import com.solvd.laba.service.city.impl.RoadService;
 import com.solvd.laba.service.navigator.INavigatorService;
+import com.solvd.laba.service.route.IRouteService;
+import com.solvd.laba.service.route.impl.RouteService;
 
+import java.awt.*;
 import java.util.*;
+import java.util.List;
 
 public class NavigatorService implements INavigatorService {
 
     private Graph graph;
     private ICityService cityService;
+    private IRouteService routeService;
+    private IRoadService roadService;
 
 
     public NavigatorService() {
         cityService = new CityService();
+        routeService = new RouteService();
+        roadService = new RoadService();
         graph = new Graph();
         graph.addCities(cityService.getAllCities());
     }
 
     @Override
     public List<City> findShortestPath(City startCity, City endCity) {
-      return  graph.findShortestPath(startCity.getId(), endCity.getId());
+        Optional<Route> existingRoute = routeService.getRouteBetweenTwoCities(startCity.getId(), endCity.getId());
+        if(existingRoute.isPresent()){
+            Map<Integer, City> cityOrder = existingRoute.get().getCityOrder();
+
+            List<City> cityList = new ArrayList<>(cityOrder.values());
+
+            cityList.forEach(city -> {
+                city.setRoads(roadService.findRoadsByStartCityId(city.getId()));
+            });
+
+            return cityList;
+
+        } else {
+            Route route = new Route();
+            Map<Integer, City> cityOrder = new HashMap<>();
+
+            int order = 1;
+
+            List<City> cities =  graph.findShortestPath(startCity.getId(), endCity.getId());
+
+            for (City city : cities) {
+                cityOrder.put(order, city);
+                order++;
+            }
+
+            route.setCityOrder(cityOrder);
+            route.setDistance(getRoadLength(cities));
+            routeService.createRoute(route);
+
+            return cities;
+        }
     }
 
     @Override
