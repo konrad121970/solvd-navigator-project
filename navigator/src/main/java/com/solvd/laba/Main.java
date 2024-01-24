d0191219e626972593f25fd37fd7321f3ffe1485
konrad121970 <konrad121970@windowslive.com>
2024-01-24 23:27:36 +0100

    Update main

--- navigator/src/main/java/com/solvd/laba/Main.java
+++ navigator/src/main/java/com/solvd/laba/Main.java
@@ -2,11 +2,14 @@ package com.solvd.laba;
 
 import com.solvd.laba.model.City;
 import com.solvd.laba.model.Route;
+import com.solvd.laba.persistence.route.IRouteCityRepository;
 import com.solvd.laba.service.city.ICityService;
 import com.solvd.laba.service.city.impl.CityService;
 import com.solvd.laba.service.navigator.INavigatorService;
 import com.solvd.laba.service.navigator.impl.NavigatorService;
+import com.solvd.laba.service.route.IRouteCityService;
 import com.solvd.laba.service.route.IRouteService;
+import com.solvd.laba.service.route.impl.RouteCityService;
 import com.solvd.laba.service.route.impl.RouteService;
 
 import java.util.List;
@@ -18,27 +21,16 @@ public class Main {
         ICityService cityService = new CityService();
         INavigatorService navigatorService = new NavigatorService();
         IRouteService routeService = new RouteService();
+        IRouteCityService routeCityService = new RouteCityService();
 
         City city1 = cityService.findCityById(1L);
-        City city2 = cityService.findCityById(5L);
+        City city2 = cityService.findCityById(4L);
         City city3 = cityService.findCityById(3L);
 
         List<City> asd = navigatorService.findShortestPath(city1, city2);
-
         List<City> asd2 = navigatorService.findShortestPathWithStop(city1, city2, city3);
 
-        try{
-            Optional<Route> route = routeService.getRouteBetweenTwoCities(1L, 5L);
-            System.out.println("Saved route: " + route.get());
-        } catch (NullPointerException e){
-            System.out.println("There is no such saved route");
-        }
-
-
-
-
-
-        System.out.println(navigatorService.getRoadLength(navigatorService.findShortestPath(city1, city2)));
+        //System.out.println(navigatorService.getRoadLength(navigatorService.findShortestPath(city1, city2)));
         System.out.println();
 
     }
