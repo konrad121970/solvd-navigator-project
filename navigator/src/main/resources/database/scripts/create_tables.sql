ee1be45b4d695ceb0787499834c7f3aa5ecd42a7
konrad121970 <konrad121970@windowslive.com>
2024-01-24 14:26:56 +0100

    Add routes table to database schema and update create_tables.sql

--- navigator/src/main/resources/database/scripts/create_tables.sql
+++ navigator/src/main/resources/database/scripts/create_tables.sql
@@ -4,7 +4,7 @@ create database if not exists navigator_db;
 
 use navigator_db;
 
-CREATE TABLE cities (
+CREATE TABLE IF NOT EXISTS cities (
     id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE, -- SERIAL
     name VARCHAR(50) NOT NULL,
     x_pos DOUBLE NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE cities (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE roads (
+CREATE TABLE IF NOT EXISTS roads (
     start_city_id BIGINT UNSIGNED NOT NULL,
     end_city_id BIGINT UNSIGNED NOT NULL ,
     distance INT NOT NULL,
@@ -21,18 +21,19 @@ CREATE TABLE roads (
     CONSTRAINT fk_roads_end_city_id FOREIGN KEY (end_city_id) REFERENCES cities(id) on delete cascade on update no action
 );
 
--- CREATE TABLE IF NOT EXISTS routes (
---     id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
---     distance INT NOT NULL,
---     PRIMARY KEY (id)
--- );
+CREATE TABLE IF NOT EXISTS routes (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
+    distance INT NOT NULL,
+    PRIMARY KEY (id)
+);
 
--- CREATE TABLE IF NOT EXISTS cities_has_routes (
---     cities_id BIGINT UNSIGNED NOT NULL,
---     routes_id BIGINT UNSIGNED NOT NULL,
---     PRIMARY KEY (cities_id, routes_id),
---     CONSTRAINT fk_cities_has_routes FOREIGN KEY (cities_id) REFERENCES
--- 	cities (id) on delete cascade on update no action,
---     CONSTRAINT fk_cities_has_routes2 FOREIGN KEY (routes_id) REFERENCES
--- 	routes (id) on delete cascade on update no action
--- );
+CREATE TABLE IF NOT EXISTS routes_has_cities (
+	routes_id BIGINT UNSIGNED NOT NULL,
+    cities_id BIGINT UNSIGNED NOT NULL,
+	city_order INT NOT NULL,
+    PRIMARY KEY (cities_id, routes_id),
+    CONSTRAINT fk_cities_has_routes FOREIGN KEY (cities_id) REFERENCES
+	cities (id) on delete cascade on update no action,
+    CONSTRAINT fk_cities_has_routes2 FOREIGN KEY (routes_id) REFERENCES
+	routes (id) on delete cascade on update no action
+);
